6b0fad5b50add2f26ab6933caa668ae9bb7f7f8f
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-28 11:21:03 +0530

    setup: primsa, better-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./db";
+import { admin } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.AUTH_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    },
+  },
+  plugins: [admin()],
+});
