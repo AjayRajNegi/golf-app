c7f400661f5010c7a58cfa4410351c53c58eb7f4
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-28 12:50:04 +0530

    feat(auth): signup endpoint working

---
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { useState, useTransition } from "react";
+import { Loader, Loader2, Send } from "lucide-react";
+import { loginWithEmailAction } from "@/app/actions/auth.actions";
+
+export default function LoginForm() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailPending, startEmailTransition] = useTransition();
+  const [githubPending, startGithubTransition] = useTransition();
+
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected...");
+          },
+          onError: () => {
+            toast.error("Internal server error.");
+          },
+        },
+      });
+    });
+  }
+  function signInWithEmail() {
+    startEmailTransition(async () => {
+      const result = await loginWithEmailAction(email, password);
+
+      if (!result.success) {
+        console.log(result.error);
+        toast.error(result.error);
+        return;
+      }
+
+      toast.success(result.message);
+      router.push("/");
+    });
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>
+          Login with your Github or Email Account
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="flex flex-col gap-4">
+        <Button
+          onClick={signInWithGithub}
+          disabled={githubPending}
+          className="w-full"
+          variant="outline"
+        >
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              {/* <GithubIcon className="size-4" />  */}
+              Sign in with Github
+            </>
+          )}
+        </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Password</Label>
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              required
+            />
+          </div>
+
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Loader2 className="size-4 animate-spin" />{" "}
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Send />
+                <span> Continue with Email</span>
+              </>
+            )}
+          </Button>
+        </div>
+      </CardContent>
+
+      <button
+        onClick={() => {
+          console.log("Heel");
+          toast.success("Hello");
+        }}
+      >
+        button
+      </button>
+    </Card>
+  );
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import SignupForm from "./_components/SignupForm";
+
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    redirect("/");
+  }
+  console.log(session);
+  return <SignupForm />;
+}
