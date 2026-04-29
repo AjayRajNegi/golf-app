6aeca762c7b2de9c23d3c5deb3fcc8ae370b4da9
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-04-29 23:48:30 +0530

    prod: updated better auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { admin } from "better-auth/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   plugins: [admin()],
 });
